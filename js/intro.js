93a72e96642296665b9f84f217643132384a1689
Sakis Kasampalis <s.kasampalis@zoho.com>
2018-11-25 19:41:40 +0100

    New index
    Update index page
    Update book reading section

---
+++ js/intro.js
@@ -0,0 +1,22 @@
+const numStars = 100;
+
+for (let i = 0; i < numStars; i++) {
+  let star = document.createElement("div");  
+  star.className = "star";
+  var xy = getRandomPosition();
+  star.style.top = xy[0] + 'px';
+  star.style.left = xy[1] + 'px';
+  var size = Math.floor(Math.random() * 3 + 1);
+  star.style.width = size + 'px';
+  star.style.height = size + 'px';
+  document.body.append(star);
+}
+
+// Gets random x, y values based on the size of the container
+function getRandomPosition() {  
+  var y = window.innerWidth;
+  var x = window.innerHeight;
+  var randomX = Math.floor(Math.random() * x);
+  var randomY = Math.floor(Math.random() * y);
+  return [randomX,randomY];
+}
